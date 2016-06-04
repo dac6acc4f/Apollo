abe59ed5046f2cd652fb6d963f30e11a50076291
NycuRO <nghergu@yahoo.com>
2016-06-04 21:38:17 +0300

    Update Installer.php

--- src/pocketmine/wizard/Installer.php
+++ src/pocketmine/wizard/Installer.php
@@ -38,7 +38,7 @@ class Installer{
 	private $defaultLang;
 
 	public function __construct(){
-		echo "[*] PocketMine-MP set-up wizard\n";
+		echo "[*] Apollo set-up wizard\n";
 		echo "[*] Please select a language:\n";
 		foreach(InstallerLang::$languages as $short => $native){
 			echo " $native => $short\n";
