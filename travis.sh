8ee10bff4b4d047f57dc4bf238d8844e06fc066b
SalmonDE <salmonde@salmonde.de>
2016-06-05 12:16:37 +0200

    Travis don't be so rude to @NycuRO

--- travis.sh
+++ travis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 find . -name "*.php" -print0 | xargs -0 -n1 php -l || exit 1
-echo -e "version\nms\nstop\n\n" | php src/pocketmine/PocketMine.php --no-wizard
-if ls plugins/Genisys/Genisys*.phar >/dev/null 2>&1; then
-    echo "Server packaged successfully."
+echo -e "ms\nstop\n\n" | php src/pocketmine/PocketMine.php --no-wizard
+if ls plugins/Apollo/Apollo*.phar >/dev/null 2>&1; then
+    echo "Apollo.phar successfully created!"
 else
-    echo "No phar created!"
+    echo "Apollo.phar wasn't able to be created!"
     exit 1
 fi
