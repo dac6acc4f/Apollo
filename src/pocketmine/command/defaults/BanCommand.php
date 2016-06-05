0530d78f1d413585d346dad16219ebcea112dfb4
NycuRO <nghergu@yahoo.com>
2016-06-05 11:01:57 +0300

    Update BanCommand.php

--- src/pocketmine/command/defaults/BanCommand.php
+++ src/pocketmine/command/defaults/BanCommand.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____
@@ -18,17 +17,12 @@
  *
  *
 */
-
 namespace pocketmine\command\defaults;
-
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\TranslationContainer;
 use pocketmine\Player;
-
-
 class BanCommand extends VanillaCommand{
-
 	public function __construct($name){
 		parent::__construct(
 			$name,
@@ -37,18 +31,14 @@ class BanCommand extends VanillaCommand{
 		);
 		$this->setPermission("pocketmine.command.ban.player");
 	}
-
 	public function execute(CommandSender $sender, $currentAlias, array $args){
 		if(!$this->testPermission($sender)){
 			return true;
 		}
-
 		if(count($args) === 0){
 			$sender->sendMessage(new TranslationContainer("commands.generic.usage", [$this->usageMessage]));
-
 			return false;
 		}
-
 		$name = array_shift($args);
 		if(isset($args[0]) and isset($args[1])){
 			$reason = $args[0];
@@ -57,18 +47,13 @@ class BanCommand extends VanillaCommand{
 			}else{
 				$until = null;
 			}
-
 			$sender->getServer()->getNameBans()->addBan($name, $reason, $until, $sender->getName());
 		}
 		$sender->getServer()->getNameBans()->addBan($name, $reason = implode(" ", $args), null, $sender->getName());
-
-
 		if(($player = $sender->getServer()->getPlayerExact($name)) instanceof Player){
 			$player->kick($reason !== "" ? "Banned by admin. Reason: " . $reason : "Banned by admin." . "Banned Until:" . date('r'), $until = "Forever");
 		}
-
 		Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.ban.success", [$player !== null ? $player->getName() : $name]));
-
 		return true;
 	}
 }
