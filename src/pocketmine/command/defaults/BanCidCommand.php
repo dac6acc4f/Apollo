ae9b2cb7d0b72261bea8a02971c5502fae604dd9
NycuRO <nghergu@yahoo.com>
2016-06-05 10:58:15 +0300

    Update BanCidCommand.php

--- src/pocketmine/command/defaults/BanCidCommand.php
+++ src/pocketmine/command/defaults/BanCidCommand.php
@@ -1,14 +1,11 @@
 <?php
 namespace pocketmine\command\defaults;
-
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\TranslationContainer;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
-
 class BanCidCommand extends VanillaCommand{
-
 	public function __construct($name){
 		parent::__construct(
 			$name,
@@ -17,25 +14,18 @@ class BanCidCommand extends VanillaCommand{
 		);
 		$this->setPermission("pocketmine.command.bancid");
 	}
-
 	public function execute(CommandSender $sender, $currentAlias, array $args){
 		if(!$this->testPermission($sender)){
-			return \true;
+			return true;
 		}
-
-		if(\count($args) === 0){
+		if(count($args) === 0){
 			$sender->sendMessage(new TranslationContainer("commands.generic.usage", [$this->usageMessage]));
-
-			return \false;
+			return false;
 		}
-
-		$cid = \array_shift($args);
-		$reason = \implode(" ", $args);
-
-		$sender->getServer()->getCIDBans()->addBan($cid, $reason, \null, $sender->getName());
-
-		$player = \null;
-
+		$cid = array_shift($args);
+		$reason = implode(" ", $args);
+		$sender->getServer()->getCIDBans()->addBan($cid, $reason, null, $sender->getName());
+		$player = null;
 		foreach($sender->getServer()->getOnlinePlayers() as $p){
 			if($p->getClientId() == $cid) {
 				$p->kick($reason !== "" ? "Banned by admin. Reason:" . $reason : "Banned by admin.");
@@ -43,9 +33,7 @@ class BanCidCommand extends VanillaCommand{
 				break;
 			}
 		}
-
-		Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.bancid.success", [$player !== \null ? $player->getName() : $cid]));
-
-		return \true;
+		Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.bancid.success", [$player !== null ? $player->getName() : $cid]));
+		return true;
 	}
 }
