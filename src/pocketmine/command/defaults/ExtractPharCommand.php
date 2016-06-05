3a7b62e3db14504268c1742d5d7f0397fef8d897
NycuRO <nghergu@yahoo.com>
2016-06-05 11:38:08 +0300

    Update ExtractPharCommand.php

--- src/pocketmine/command/defaults/ExtractPharCommand.php
+++ src/pocketmine/command/defaults/ExtractPharCommand.php
@@ -1,11 +1,8 @@
 <?php
 namespace pocketmine\command\defaults;
-
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
-
 class ExtractPharCommand extends VanillaCommand{
-
 	public function __construct($name){
 		parent::__construct(
 			$name,
@@ -14,12 +11,10 @@ class ExtractPharCommand extends VanillaCommand{
 		);
 		$this->setPermission("pocketmine.command.extractphar");
 	}
-
 	public function execute(CommandSender $sender, $commandLabel, array $args){
 		if(!$this->testPermission($sender)){
 			return false;
 		}
-
 		if(count($args) === 0){
 			$sender->sendMessage(TextFormat::RED . "Usage: ".$this->usageMessage);
 			return true;
@@ -33,7 +28,6 @@ class ExtractPharCommand extends VanillaCommand{
 		}
 		
 		$pharPath = "phar://$args[0]";
-
 		foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($pharPath)) as $fInfo){
 			$path = $fInfo->getPathname();
 			@mkdir(dirname($folderPath . str_replace($pharPath, "", $path)), 0755, true);
