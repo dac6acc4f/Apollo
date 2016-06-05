0e82ab02138ce62495e60c9ca73b6ef0741f861f
Aericio <Aericio@users.noreply.github.com>
2016-06-05 09:50:27 -1000

    Update ChunkInfoCommand.php

--- src/pocketmine/command/defaults/ChunkInfoCommand.php
+++ src/pocketmine/command/defaults/ChunkInfoCommand.php
@@ -1,23 +1,6 @@
 <?php
-/*
- *
- *  _____   _____   __   _   _   _____  __    __  _____
- * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
- * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
- * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
- * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
- * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author iTX Technologies
- * @link https://mcper.cn
- *
- */
 namespace pocketmine\command\defaults;
+
 use pocketmine\command\CommandSender;
 use pocketmine\event\TranslationContainer;
 use pocketmine\level\format\mcregion\McRegion;
@@ -25,6 +8,7 @@ use pocketmine\level\Level;
 use pocketmine\level\Position;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
+
 class ChunkInfoCommand extends VanillaCommand{
 	public function __construct($name){
 		parent::__construct(
