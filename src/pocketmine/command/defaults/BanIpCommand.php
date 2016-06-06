4bd0b9dd338b7def7c910a6eb3deb53c1ee79e64
NycuRO <nghergu@yahoo.com>
2016-06-07 00:09:16 +0300

    @Aericio

--- src/pocketmine/command/defaults/BanIpCommand.php
+++ src/pocketmine/command/defaults/BanIpCommand.php
@@ -1,14 +1,10 @@
 <?php
 namespace pocketmine\command\defaults;
-
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\TranslationContainer;
 use pocketmine\Player;
-
-
 class BanIpCommand extends VanillaCommand{
-
 	public function __construct($name){
 		parent::__construct(
 			$name,
@@ -17,49 +13,39 @@ class BanIpCommand extends VanillaCommand{
 		);
 		$this->setPermission("pocketmine.command.ban.ip");
 	}
-
 	public function execute(CommandSender $sender, $currentAlias, array $args){
 		if(!$this->testPermission($sender)){
 			return true;
 		}
-
 		if(count($args) === 0){
 			$sender->sendMessage(new TranslationContainer("commands.generic.usage", [$this->usageMessage]));
 
 			return false;
 		}
-
 		$value = array_shift($args);
 		$reason = implode(" ", $args);
 
 		if(preg_match("/^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$/", $value)){
 			$this->processIPBan($value, $sender, $reason);
-
 			Command::broadcastCommandMessage($sender, new TranslationContainer("commands.banip.success", [$value]));
 		}else{
 			if(($player = $sender->getServer()->getPlayer($value)) instanceof Player){
 				$this->processIPBan($player->getAddress(), $sender, $reason);
-
 				Command::broadcastCommandMessage($sender, new TranslationContainer("commands.banip.success.players", [$player->getAddress(), $player->getName()]));
 			}else{
 				$sender->sendMessage(new TranslationContainer("commands.banip.invalid"));
-
 				return false;
 			}
 		}
-
 		return true;
 	}
-
 	private function processIPBan($ip, CommandSender $sender, $reason){
 		$sender->getServer()->getIPBans()->addBan($ip, $reason, null, $sender->getName());
-
 		foreach($sender->getServer()->getOnlinePlayers() as $player){
 			if($player->getAddress() === $ip){
-				$player->kick($reason !== "" ? $reason : "IP banned.");
+				$player->kick($reason !== "" ? $reason : "You have BANNED-IP.");
 			}
 		}
-
 		$sender->getServer()->getNetwork()->blockAddress($ip, -1);
 	}
 }
