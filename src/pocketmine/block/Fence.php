d0dc014b69872c648ee763a4f64ed0608e55ca75
NycuRO <nghergu@yahoo.com>
2016-06-08 20:58:05 +0300

    Line

--- src/pocketmine/block/Fence.php
+++ src/pocketmine/block/Fence.php
@@ -3,37 +3,29 @@ namespace pocketmine\block;
 use pocketmine\item\Tool;
 use pocketmine\math\AxisAlignedBB;
 use pocketmine\math\Vector3;
-class Fence extends Transparent{
-	
+class Fence extends Transparent{	
 	const FENCE_OAK = 0;  
 	const FENCE_SPRUCE = 1;
 	const FENCE_BIRCH = 2;
 	const FENCE_JUNGLE = 3;
 	const FENCE_ACACIA = 4;
 	const FENCE_DARKOAK = 5;
-
 	protected $id = self::FENCE;
-
 	public function __construct($meta = 0){
 		$this->meta = $meta;
 	}
-
 	public function getHardness() {
 		return 2;
 	}
-
 	public function getToolType(){
 		return Tool::TYPE_AXE;
 	}
-
 	public function getBurnChance() {
 		return 5;
 	}
-
 	public function getBurnAbility() {
 		return 20;
 	}
-
 	public function getName(){
 		static $names = [
 			0 => "Oak Fence",
@@ -47,19 +39,15 @@ class Fence extends Transparent{
 		];
 		return $names[$this->meta & 0x07];
 	}
-
 	protected function recalculateBoundingBox() {
-
 		$north = $this->canConnect($this->getSide(Vector3::SIDE_NORTH));
 		$south = $this->canConnect($this->getSide(Vector3::SIDE_SOUTH));
 		$west = $this->canConnect($this->getSide(Vector3::SIDE_WEST));
 		$east = $this->canConnect($this->getSide(Vector3::SIDE_EAST));
-
 		$n = $north ? 0 : 0.375;
 		$s = $south ? 1 : 0.625;
 		$w = $west ? 0 : 0.375;
 		$e = $east ? 1 : 0.625;
-
 		return new AxisAlignedBB(
 			$this->x + $w,
 			$this->y,
@@ -69,9 +57,7 @@ class Fence extends Transparent{
 			$this->z + $s
 		);
 	}
-
 	public function canConnect(Block $block){
 		return ($block instanceof Fence or $block instanceof FenceGate) ? true : $block->isSolid() and !$block->isTransparent();
 	}
-
 }
