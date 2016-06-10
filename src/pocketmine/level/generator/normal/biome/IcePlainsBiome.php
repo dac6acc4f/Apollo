b4a1e62924a4a8edef50b991ef3796c6f073f595
NycuRO <nghergu@yahoo.com>
2016-06-10 15:44:47 +0300

    Update IcePlainsBiome.php

--- src/pocketmine/level/generator/normal/biome/IcePlainsBiome.php
+++ src/pocketmine/level/generator/normal/biome/IcePlainsBiome.php
@@ -1,11 +1,16 @@
 <?php
 namespace pocketmine\level\generator\normal\biome;
-use pocketmine\level\generator\populator\TallGrass;
-class IcePlainsBiome extends SnowyBiome{
+use pocketmine\block\Block;
+class IcePlainsBiome extends GrassyBiome{
 	public function __construct(){
 		parent::__construct();
-		$tallGrass = new TallGrass();
-		$tallGrass->setBaseAmount(5);
+		$this->setGroundCover([
+			Block::get(Block::SNOW_LAYER, 0),
+			Block::get(Block::GRASS, 0),
+			Block::get(Block::DIRT, 0),
+			Block::get(Block::DIRT, 0),
+			Block::get(Block::DIRT, 0),
+		]);
 		$this->addPopulator($tallGrass);
 		$this->setElevation(56, 74);
 		$this->temperature = 0.05;
