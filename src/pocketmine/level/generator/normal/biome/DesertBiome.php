3d6c8407bfdd2bf094b155a143c3269379e76157
NycuRO <nghergu@yahoo.com>
2016-06-10 15:40:14 +0300

    Update DesertBiome.php

--- src/pocketmine/level/generator/normal/biome/DesertBiome.php
+++ src/pocketmine/level/generator/normal/biome/DesertBiome.php
@@ -1,14 +1,35 @@
 <?php
 namespace pocketmine\level\generator\normal\biome;
-class DesertBiome extends SandyBiome{
+use pocketmine\block\Block;
+use pocketmine\level\generator\populator\Cactus;
+use pocketmine\level\generator\populator\TallCacti;
+use pocketmine\level\generator\populator\DeadBush;
+class DesertBiome extends GrassyBiome{
 	public function __construct(){
 		parent::__construct();
-		$this->setElevation(56, 74);
-
-		$this->temperature = 2;
-		$this->rainfall = 0;
+		$cactus = new Cactus();
+		$cactus->setBaseAmount(1);
+		$tallCacti = new TallCacti();
+		$tallCacti->setBaseAmount(1);
+		$deadBush = new DeadBush();
+		$deadBush->setBaseAmount(1);
+		$this->addPopulator($cactus);
+		$this->addPopulator($tallCacti);
+		$this->addPopulator($deadBush);
+		$this->temperature = 2.0;
+		$this->rainfall = 0.0;
+		$this->setGroundCover([
+			Block::get(Block::SAND, 0),
+			Block::get(Block::SAND, 0),
+			Block::get(Block::SAND, 0),
+			Block::get(Block::SANDSTONE, 0),
+			Block::get(Block::SANDSTONE, 0),
+			Block::get(Block::SANDSTONE, 0),
+			Block::get(Block::SANDSTONE, 0),
+			Block::get(Block::SANDSTONE, 0),
+		]);
 	}
-	public function getName(){
+	public function getName() {
 		return "Desert";
 	}
 }
