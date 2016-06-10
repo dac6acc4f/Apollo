0ffc8d4e07497709e3ee32724894fccc1b318738
NycuRO <nghergu@yahoo.com>
2016-06-10 21:52:15 +0300

    Update RiverBiome.php

--- src/pocketmine/level/generator/normal/biome/RiverBiome.php
+++ src/pocketmine/level/generator/normal/biome/RiverBiome.php
@@ -3,6 +3,8 @@ namespace pocketmine\level\generator\normal\biome;
 use pocketmine\level\generator\populator\Sugarcane;
 use pocketmine\level\generator\populator\TallGrass;
 use pocketmine\level\generator\populator\TallSugarcane;
+use pocketmine\block\Sapling;
+use pocketmine\level\generator\populator\Tree;
 class RiverBiome extends GrassyBiome{
 	public function __construct(){
 		parent::__construct();
@@ -12,9 +14,12 @@ class RiverBiome extends GrassyBiome{
 		$tallSugarcane->setBaseAmount(60);
 		$tallGrass = new TallGrass();
 		$tallGrass->setBaseAmount(5);
+		$trees = new Tree(Sapling::SPRUCE);
+		$trees->setBaseAmount(1);
 		$this->addPopulator($sugarcane);
 		$this->addPopulator($tallSugarcane);
 		$this->addPopulator($tallGrass);
+                $this->addPopulator($trees);
 		$this->setElevation(56, 74);
 		$this->temperature = 0.5;
 		$this->rainfall = 0.7;
