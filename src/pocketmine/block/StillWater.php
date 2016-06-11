7db2e365c8b482df25933a616c92c9ad4a96f089
NycuRO <nghergu@yahoo.com>
2016-06-11 18:37:45 +0300

    Update StillWater.php

--- src/pocketmine/block/StillWater.php
+++ src/pocketmine/block/StillWater.php
@@ -1,13 +1,7 @@
 <?php
 namespace pocketmine\block;
-use pocketmine\level\Level;
 class StillWater extends Water{
 	protected $id = self::STILL_WATER;
-	public function onUpdate($type){
-		if($type == Level::BLOCK_UPDATE_NORMAL){
-			parent::onUpdate($type);
-		}
-	}
 	public function getName(){
 		return "Still Water";
 	}
