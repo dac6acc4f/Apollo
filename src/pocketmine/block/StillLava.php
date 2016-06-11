79f57bf71e835afe4a562fb3e2b52b72ec8a53e9
NycuRO <nghergu@yahoo.com>
2016-06-11 18:36:48 +0300

    Update StillLava.php

--- src/pocketmine/block/StillLava.php
+++ src/pocketmine/block/StillLava.php
@@ -1,13 +1,7 @@
 <?php
 namespace pocketmine\block;
-use pocketmine\level\Level;
 class StillLava extends Lava{
 	protected $id = self::STILL_LAVA;
-	public function onUpdate($type){
-		if($type == Level::BLOCK_UPDATE_NORMAL){
-			parent::onUpdate($type);
-		}
-	}
 	public function getName(){
 		return "Still Lava";
 	}
