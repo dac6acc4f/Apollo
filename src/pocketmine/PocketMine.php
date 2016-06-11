05eed5c764b94a7e3ba4176f866d1af9a85df7ba
NycuRO <nghergu@yahoo.com>
2016-06-11 16:36:34 +0300

    0.4 release
    0.2 and 0.3 no exist ;)

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -52,7 +52,7 @@ namespace pocketmine {
 	use pocketmine\utils\Utils;
 	use pocketmine\wizard\Installer;
 
-	const VERSION = "0.1dev";
+	const VERSION = "0.4";
 	const API_VERSION = "1.13.1";
 	const CODENAME = "NycuRO";
 	const MINECRAFT_VERSION = "v0.14.3 alpha";
