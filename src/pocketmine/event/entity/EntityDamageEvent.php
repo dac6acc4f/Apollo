ea6aebeb973d3633a8c132206201970003b3ab96
NycuRO <nghergu@yahoo.com>
2016-06-12 14:24:04 +0300

    Update EntityDamageEvent.php

--- src/pocketmine/event/entity/EntityDamageEvent.php
+++ src/pocketmine/event/entity/EntityDamageEvent.php
@@ -1,40 +1,15 @@
 <?php
-
-/**
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link   http://www.pocketmine.net/
- *
- *
- */
-
 namespace pocketmine\event\entity;
-
 use pocketmine\entity\Effect;
 use pocketmine\entity\Entity;
 use pocketmine\event\Cancellable;
-
 class EntityDamageEvent extends EntityEvent implements Cancellable{
 	public static $handlerList = null;
-
 	const MODIFIER_BASE = 0;
 	const MODIFIER_ARMOR = 1;
 	const MODIFIER_STRENGTH = 2;
 	const MODIFIER_WEAKNESS = 3;
 	const MODIFIER_RESISTANCE = 4;
-
-	const CAUSE_CONTACT = 0;
 	const CAUSE_ENTITY_ATTACK = 1;
 	const CAUSE_PROJECTILE = 2;
 	const CAUSE_SUFFOCATION = 3;
@@ -49,24 +24,10 @@ class EntityDamageEvent extends EntityEvent implements Cancellable{
 	const CAUSE_SUICIDE = 12;
 	const CAUSE_MAGIC = 13;
 	const CAUSE_CUSTOM = 14;
-	const CAUSE_STARVATION = 15;
-
-	const CAUSE_LIGHTNING = 16;
-
-
+        const CAUSE_CONTACT = 15;
 	private $cause;
-	/** @var array */
 	private $modifiers;
 	private $originals;
-
-
-	/**
-	 * @param Entity    $entity
-	 * @param int       $cause
-	 * @param int|int[] $damage
-	 *
-	 * @throws \Exception
-	 */
 	public function __construct(Entity $entity, $cause, $damage){
 		$this->entity = $entity;
 		$this->cause = $cause;
@@ -77,80 +38,40 @@ class EntityDamageEvent extends EntityEvent implements Cancellable{
 				self::MODIFIER_BASE => $damage
 			];
 		}
-
 		$this->originals = $this->modifiers;
-
 		if(!isset($this->modifiers[self::MODIFIER_BASE])){
 			throw new \InvalidArgumentException("BASE Damage modifier missing");
 		}
-
 		if($entity->hasEffect(Effect::DAMAGE_RESISTANCE)){
 			$this->setDamage(-($this->getDamage(self::MODIFIER_BASE) * 0.20 * ($entity->getEffect(Effect::DAMAGE_RESISTANCE)->getAmplifier() + 1)), self::MODIFIER_RESISTANCE);
 		}
 	}
-
-	/**
-	 * @return int
-	 */
 	public function getCause(){
 		return $this->cause;
 	}
-
-	/**
-	 * @param int $type
-	 *
-	 * @return int
-	 */
 	public function getOriginalDamage($type = self::MODIFIER_BASE){
 		if(isset($this->originals[$type])){
 			return $this->originals[$type];
 		}
-
 		return 0;
 	}
-
-	/**
-	 * @param int $type
-	 *
-	 * @return int
-	 */
 	public function getDamage($type = self::MODIFIER_BASE){
 		if(isset($this->modifiers[$type])){
 			return $this->modifiers[$type];
 		}
-
 		return 0;
 	}
-
-	/**
-	 * @param float $damage
-	 * @param int   $type
-	 *
-	 * @throws \UnexpectedValueException
-	 */
 	public function setDamage($damage, $type = self::MODIFIER_BASE){
 		$this->modifiers[$type] = $damage;
 	}
-
-	/**
-	 * @param int $type
-	 *
-	 * @return bool
-	 */
 	public function isApplicable($type){
 		return isset($this->modifiers[$type]);
 	}
-
-	/**
-	 * @return int
-	 */
 	public function getFinalDamage(){
 		$damage = 0;
 		foreach($this->modifiers as $type => $d){
 			$damage += $d;
 		}
-
 		return $damage;
 	}
-
 }
