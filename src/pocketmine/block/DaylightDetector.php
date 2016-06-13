4835cce33455a3ea48854828d0a004c133250480
NycuRO <nghergu@yahoo.com>
2016-06-13 22:36:23 +0300

    Update DaylightDetector.php

--- src/pocketmine/block/DaylightDetector.php
+++ src/pocketmine/block/DaylightDetector.php
@@ -1,69 +1,86 @@
 <?php
+/*
+ *
+ *  _____   _____   __   _   _   _____  __    __  _____
+ * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
+ * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
+ * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
+ * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
+ * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author iTX Technologies
+ * @link https://mcper.cn
+ *
+ */
 namespace pocketmine\block;
 use pocketmine\item\Item;
-use pocketmine\level\Level;
 use pocketmine\Player;
-use pocketmine\math\AxisAlignedBB;
-use pocketmine\tile\DLDetector;
-use pocketmine\nbt\tag\Compound as CompoundTag;
-use pocketmine\nbt\tag\Enum as ListTag;
-use pocketmine\nbt\tag\String as StringTag;
-use pocketmine\nbt\tag\Int as IntTag;
+use pocketmine\nbt\tag\Compound;
+use pocketmine\nbt\tag\String;
+use pocketmine\nbt\tag\Int;
 use pocketmine\tile\Tile;
-class DaylightDetector extends Transparent implements RedstoneSource{
+use pocketmine\tile\DLDetector;
+class DaylightDetector extends RedstoneSource{
 	protected $id = self::DAYLIGHT_DETECTOR;
-	public function __construct($meta = 0){
-		$this->meta = $meta;
-	}
+	//protected $hasStartedUpdate = false;
 	public function getName(){
 		return "Daylight Detector";
 	}
-	public function isRedstone(){
-		return true;
+	public function getBoundingBox(){
+		if($this->boundingBox === null){
+			$this->boundingBox = $this->recalculateBoundingBox();
+		}
+		return $this->boundingBox;
+	}
+	public function canBeFlowedInto(){
+		return false;
 	}
 	public function canBeActivated(){
 		return true;
-	}	
-	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
-		$this->getLevel()->setBlock($block, $this, true);
-		$nbt = new CompoundTag("", [
-				new StringTag("id", Tile::DAY_LIGHT_DETECTOR),
-				new IntTag("x", $this->x),
-				new IntTag("y", $this->y),
-				new IntTag("z", $this->z)
-		]);
-		Tile::createTile(Tile::DAY_LIGHT_DETECTOR, $this->getLevel()->getChunk($this->x >> 4, $this->z >> 4), $nbt);
-		$this->getLevel()->scheduleUpdate($this, 50);
-		
-		return true;
 	}
-	public function onUpdate($type){
-		if($type === Level::BLOCK_UPDATE_SCHEDULED || $type === Level::BLOCK_UPDATE_NORMAL){
-			$this->getLevel()->scheduleUpdate($this, 50);
-			$this->BroadcastRedstoneUpdate(Level::REDSTONE_UPDATE_NORMAL, $this->getPower());
+	/**
+	 * @return DLDetector
+	 */
+	protected function getTile(){
+		$t = $this->getLevel()->getTile($this);
+		if($t instanceof DLDetector){
+			return $t;
+		}else{
+			$nbt = new Compound("", [
+				new String("id", Tile::DAY_LIGHT_DETECTOR),
+				new Int("x", $this->x),
+				new Int("y", $this->y),
+				new Int("z", $this->z)
+			]);
+			return Tile::createTile(Tile::DAY_LIGHT_DETECTOR, $this->getLevel()->getChunk($this->x >> 4, $this->z >> 4), $nbt);
 		}
-		return false;
 	}
 	public function onActivate(Item $item, Player $player = null){
-		$this->id = self::DAYLIGHT_DETECTOR_INVERTED;
-		$this->getLevel()->setBlock($this, $this, true);
-		$this->BroadcastRedstoneUpdate(Level::REDSTONE_UPDATE_NORMAL, 16 - $this->getPower());
+		$this->getLevel()->setBlock($this, new DaylightDetectorInverted(), true, true);
+		$this->getTile()->onUpdate();
 		return true;
 	}
-	public function getDrops(Item $item){
-		return [[self::DAYLIGHT_DETECTOR,0,1]];
+	public function isActivated(Block $from = null){
+		return $this->getTile()->isActivated();
 	}
-	public function getPower(){
-		return $this->getLevel()->getTile($this) instanceof DLDetector ? $this->getLevel()->getTile($this)->getPower() : 0;
+	public function onBreak(Item $item){
+		$this->getLevel()->setBlock($this, new Air());
+		if($this->isActivated()) $this->deactivate();
 	}
-	protected function recalculateBoundingBox(){
-		return new AxisAlignedBB(
-			$this->x,
-			$this->y,
-			$this->z,
-			$this->x + 1,
-			$this->y + 0.375,
-			$this->z + 1
-		);
+	public function getHardness() {
+		return 0.2;
+	}
+	public function getResistance(){
+		return 1;
+	}
+	public function getDrops(Item $item) {
+		return [
+			[self::DAYLIGHT_SENSOR, 0, 1]
+		];
 	}
 }
