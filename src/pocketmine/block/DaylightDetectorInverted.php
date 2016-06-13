746503e0080f76f8e3ccfacd361fc196a3bdc56a
NycuRO <nghergu@yahoo.com>
2016-06-13 22:54:02 +0300

    Update DaylightDetectorInverted.php

--- src/pocketmine/block/DaylightDetectorInverted.php
+++ src/pocketmine/block/DaylightDetectorInverted.php
@@ -1,56 +1,30 @@
 <?php
+/*
+ *
+ *  _____   _____   __   _   _   _____  __    __  _____
+ * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
+ * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
+ * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
+ * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
+ * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author iTX Technologies
+ * @link https://mcper.cn
+ *
+ */
 namespace pocketmine\block;
 use pocketmine\item\Item;
-use pocketmine\level\Level;
 use pocketmine\Player;
-use pocketmine\math\AxisAlignedBB;
-class DaylightDetectorInverted extends Transparent implements Redstone, RedstoneSwitch{
+class DaylightDetectorInverted extends DaylightDetector{
 	protected $id = self::DAYLIGHT_DETECTOR_INVERTED;
-	public function __construct($meta = 0){
-		$this->meta = $meta;
-	}
-	public function getName(){
-		return "Inverted Daylight Detector";
-	}
-	public function isRedstone(){
-		return true;
-	}
-	public function canBeActivated(){
-		return true;
-	}
-	public function onUpdate($type){
-		if($type === Level::BLOCK_UPDATE_SCHEDULED || $type === Level::BLOCK_UPDATE_NORMAL){
-			$this->getLevel()->scheduleUpdate($this, 50);
-			$this->BroadcastRedstoneUpdate(Level::REDSTONE_UPDATE_NORMAL, $this->getPower());
-		}
-		return false;
-	}
 	public function onActivate(Item $item, Player $player = null){
-		$this->id = self::DAYLIGHT_DETECTOR;
-		$this->getLevel()->setBlock($this, $this, true);
-		$this->BroadcastRedstoneUpdate(Level::REDSTONE_UPDATE_NORMAL, $this->getPower());
+		$this->getLevel()->setBlock($this, new DaylightDetector(), true, true);
+		$this->getTile()->onUpdate();
 		return true;
-	}
-	public function getDrops(Item $item){
-		return [[self::DAYLIGHT_DETECTOR,0,1]];
-	}
-	public function getPower(){
-		$fulltime = Level::TIME_FULL;
-		$time = ($fulltime + ((3000 - $this->getLevel()->getTime())) * 2);
-		if($time < 0 || $time > $fulltime) $time *= -1;
-		if($time < 0) $time += $fulltime * 2;
-		$power = (floor($time / $fulltime * 15) + 1) - (15 - $this->getLightLevel());
-		if($power < 0) $power = 0;
-		return 15 - $power;
-	}
-	protected function recalculateBoundingBox(){
-		return new AxisAlignedBB(
-			$this->x,
-			$this->y,
-			$this->z,
-			$this->x + 1,
-			$this->y + 0.375,
-			$this->z + 1
-		);
 	}
 }
