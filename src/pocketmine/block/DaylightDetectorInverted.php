03a8783f19975a45e3ac052ddabe82cf8fe0b90c
NycuRO <nghergu@yahoo.com>
2016-06-13 23:04:35 +0300

    Update DaylightDetectorInverted.php

--- src/pocketmine/block/DaylightDetectorInverted.php
+++ src/pocketmine/block/DaylightDetectorInverted.php
@@ -21,9 +21,9 @@ namespace pocketmine\block;
 use pocketmine\item\Item;
 use pocketmine\Player;
 class DaylightDetectorInverted extends DaylightDetector{
-	protected $id = self::DAYLIGHT_DETECTOR_INVERTED;
+	protected $id = self::DAYLIGHT_SENSOR_INVERTED;
 	public function onActivate(Item $item, Player $player = null){
-		$this->getLevel()->setBlock($this, new DaylightDetector(), true, true);
+		$this->getLevel()->setBlock($this, new DaylightSensor(), true, true);
 		$this->getTile()->onUpdate();
 		return true;
 	}
