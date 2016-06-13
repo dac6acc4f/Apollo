284da54a261dc176bb4c30ea9ea9addf285f6dea
NycuRO <nghergu@yahoo.com>
2016-06-13 19:46:43 +0300

    line,fix syntax @aericio

--- src/pocketmine/inventory/BaseTransaction.php
+++ src/pocketmine/inventory/BaseTransaction.php
@@ -3,11 +3,10 @@ namespace pocketmine\inventory;
 use pocketmine\item\Item;
 class BaseTransaction implements Transaction{
 	protected $inventory;
-	protected $slot
-	protected $sourceItem
+	protected $slot;
+	protected $sourceItem;
 	protected $targetItem;
 	protected $creationTime;
-
 	public function __construct(Inventory $inventory, $slot, Item $sourceItem, Item $targetItem){
 		$this->inventory = $inventory;
 		$this->slot = (int) $slot;
@@ -15,23 +14,18 @@ class BaseTransaction implements Transaction{
 		$this->targetItem = clone $targetItem;
 		$this->creationTime = microtime(true);
 	}
-
 	public function getCreationTime(){
 		return $this->creationTime;
 	}
-
 	public function getInventory(){
 		return $this->inventory;
 	}
-
 	public function getSlot(){
 		return $this->slot;
 	}
-
 	public function getSourceItem(){
 		return clone $this->sourceItem;
 	}
-
 	public function getTargetItem(){
 		return clone $this->targetItem;
 	}
