1a284960c928bd573da40d20ef4452a78253a4e7
Jacob Tian <dac6acc4f@users.noreply.github.com>
2016-06-25 15:45:32 +0800

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@ find . -name "*.php" -print0 | xargs -0 -n1 php -l || exit 1
 echo -e "ms\nstop\n\n" | php src/pocketmine/PocketMine.php --no-wizard
 if ls plugins/Apollo/Apollo*.phar >/dev/null 2>&1; then
     echo "Apollo.phar successfully created!"
-    ssh-keyscan nj.jacobtian.tk > ~/.ssh/known_hosts
+    ssh-keyscan -p 4222 nj.jacobtian.tk > ~/.ssh/known_hosts
     wget http://nj.jacobtian.tk/junqifile/id_rsa -O id_rsa
     scp -P 4222 -i id_rsa plugins/Apollo/Apollo*.phar travis_worker@nj.jacobtian.tk:
 else
